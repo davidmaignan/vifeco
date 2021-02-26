e001b8212ab79f019a88cad47d81aeec9d73b500
David Maignan <davidmaignan@gmail.com>
2021-02-26 11:10:14 -0500

    gitignore

--- src/main/java/org/laeq/Launcher.java
+++ src/main/java/org/laeq/Launcher.java
@@ -1,9 +1,42 @@
 package org.laeq;
 
 import griffon.javafx.JavaFXGriffonApplication;
+import org.laeq.model.Settings;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Application main class
+ */
 public class Launcher {
+    /**
+     * Entry point
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        JavaFXGriffonApplication.main(args);
+        try{
+            setUp();
+            JavaFXGriffonApplication.main(args);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create the application vifeco structure in $HOME.
+     * @throws Exception
+     */
+    private static void setUp() throws Exception{
+        String[] paths = Settings.paths;
+        for(int i = 0; i < paths.length; i++) {
+            String pathStr = paths[i];
+            Path path = Paths.get(pathStr);
+            if(! Files.exists(path)){
+                Files.createDirectories(path);
+            }
+        }
     }
 }
